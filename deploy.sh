0cc36fe848de110f04f16a71361b1dd312e0be1c
xuguihua <xuguihua@igetcool.com>
2025-03-22 12:37:56 +0800

    feat: 修改 sh 脚本

--- deploy.sh
+++ deploy.sh
@@ -5,26 +5,25 @@ set -e
 
 echo "===开始执行脚本==="
 
+echo "从仓库拉取代码"
+git pull
+echo "拉取代码完成"
+
+echo "================"
+
 echo "开始安装依赖"
-#/usr/bin/node -v
-#/usr/bin/npm -v
-
-# node -v
-
 npm i
-
-echo "依赖安装成功，开始打包项目"
+echo "依赖安装成功"
 
 echo "================"
 
 echo "开始打包项目"
-# 生成静态文件
 npm run build
 echo "打包完成"
 
-# 进入生成的文件夹
+echo "================"
 
-echo "正在推送至阿里云服务器，请耐心等待"
+echo "开始推送至阿里云服务器，请耐心等待"
 scp -r dist root@www.guihurge.top:/www/wwwroot/www.feblog.com
 echo "推送成功"
 
